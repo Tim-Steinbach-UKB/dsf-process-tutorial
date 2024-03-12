d85e09b032b7dff0241d2625ebca51cfdb0645bf
Tim Steinbach <Tim.Steinbach@UKBonn.de>
2024-03-12 16:30:22 +0100

    changes for ex-1

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
@@ -1,21 +1,22 @@
 package dev.dsf.process.tutorial.service;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
 import dev.dsf.bpe.v1.variables.Variables;
 
-public class DicTask extends AbstractServiceDelegate
-{
-	public DicTask(ProcessPluginApi api)
-	{
+public class DicTask extends AbstractServiceDelegate {
+	private static Logger LOGGER = LoggerFactory.getLogger(DicTask.class);
+
+	public DicTask(ProcessPluginApi api) {
 		super(api);
 	}
 
 	@Override
-	protected void doExecute(DelegateExecution execution, Variables variables)
-	{
-
+	protected void doExecute(DelegateExecution execution, Variables variables) {
+		LOGGER.info(variables.getTarget().getOrganizationIdentifierValue());
 	}
 }
--- tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
@@ -1,15 +1,20 @@
 package dev.dsf.process.tutorial.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Scope;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.fhir.authorization.read.ReadAccessHelper;
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
+import dev.dsf.process.tutorial.service.DicTask;
 
-@Configuration
-public class TutorialConfig
-{
+@Configurable
+@Scope("prototype")
+public class TutorialConfig {
+	@SuppressWarnings("unused")
 	@Autowired
 	private ProcessPluginApi api;
 
+	@ProcessDocumentation(description = "DIC Example Task")
+	private DicTask dicTask;
 }
