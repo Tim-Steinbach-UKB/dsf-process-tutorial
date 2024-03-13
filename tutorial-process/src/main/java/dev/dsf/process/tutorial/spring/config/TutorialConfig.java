a9ed291ebb1491ef7dd99127d5c8ddb406d47570
Tim Steinbach <Tim.Steinbach@UKBonn.de>
2024-03-13 11:37:56 +0100

    changes for ex-1

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
@@ -1,23 +1,25 @@
 package dev.dsf.process.tutorial.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.process.tutorial.service.DicTask;
 
-@Configurable
-public class TutorialConfig {
+@Configuration
+public class TutorialConfig
+{
 	@Autowired
 	private ProcessPluginApi api;
 
 	@Bean
-	@Scope("prototype")
-	// @ProcessDocumentation(description = "DIC Example Task")
-	public DicTask dicTask() {
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public DicTask dicTask()
+	{
 		return new DicTask(api);
-
 	}
+
 }
