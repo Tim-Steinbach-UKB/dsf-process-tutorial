74d1523d9c7094bd9f15afbc2d09ca4e441f772b
U-UKB\50114391 <UKB+50114391@UKB3352.klinik.bn>
2024-03-13 15:37:33 +0100

    changes for ex-2

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/DicTask.java
@@ -1,23 +1,40 @@
 package dev.dsf.process.tutorial.service;
 
+import java.util.Optional;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
 import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.process.tutorial.ConstantsTutorial;
 
 public class DicTask extends AbstractServiceDelegate {
 	private static Logger LOGGER = LoggerFactory.getLogger(DicTask.class);
 
-	public DicTask(ProcessPluginApi api) {
+	@Value("${dev.dsf.process.tutorial.service.loggingEnabled:false}")
+	@ProcessDocumentation(description = "Set to true to enable logging", required = false, processNames = ConstantsTutorial.PROCESS_NAME_FULL_DIC)
+	private boolean loggingEnabled;
+
+	public DicTask(ProcessPluginApi api, boolean loggingEnabled) {
 		super(api);
+		this.loggingEnabled = loggingEnabled;
 	}
 
 	@Override
 	protected void doExecute(DelegateExecution execution, Variables variables) {
-		LOGGER.info("Hello DIC from organization '{}'",
-				variables.getStartTask().getRestriction().getRecipientFirstRep().getIdentifier().getValue());
+		if (loggingEnabled) {
+			LOGGER.info("Hello DIC from organization '{}'",
+					variables.getStartTask().getRestriction().getRecipientFirstRep().getIdentifier().getValue());
+
+			Optional<String> tutorialInputParameter = api.getTaskHelper().getFirstInputParameterStringValue(
+					variables.getStartTask(), "http://dsf.dev/fhir/CodeSystem/tutorial", "Test");
+
+			LOGGER.info("Input: '{}'", tutorialInputParameter.get());
+		}
 	}
 }
--- tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
@@ -1,12 +1,15 @@
 package dev.dsf.process.tutorial.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
+import dev.dsf.process.tutorial.ConstantsTutorial;
 import dev.dsf.process.tutorial.service.DicTask;
 
 @Configuration
@@ -14,10 +17,14 @@ public class TutorialConfig {
 	@Autowired
 	private ProcessPluginApi api;
 
+	@Value("${dev.dsf.process.tutorial.service.loggingEnabled:false}")
+	@ProcessDocumentation(description = "Set to true to enable logging", required = false, processNames = ConstantsTutorial.PROCESS_NAME_FULL_DIC)
+	private boolean loggingEnabled;
+
 	@Bean
 	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public DicTask dicTask() {
-		return new DicTask(api);
+		return new DicTask(api, loggingEnabled);
 	}
 
 }
